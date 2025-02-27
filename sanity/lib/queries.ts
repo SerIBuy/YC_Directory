2e84aa8721a29b213f7182aaf82941b801e317fe
Sergey Buy <154270878+SerIBuy@users.noreply.github.com>
2025-02-27 15:43:20 +0300

    some little fix

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -3,12 +3,12 @@ import { defineQuery } from "next-sanity";
 export const STARTUPS_QUERY = defineQuery(`*[_type == 'startup' && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc){
   _id,
   title,
-  description,
-  category,
+  slug,
   _createdAt,
   author -> {_id, name, bio, image},
   views,
-  slug,
+  description,
+  category,
   image
 }`);
 
